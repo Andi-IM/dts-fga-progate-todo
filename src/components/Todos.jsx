7bc9122528b081fbe2fabf90e976e58df3e1908b
Andi-IM <andi.irhamm@gmail.com>
2024-05-28 15:33:47 +0700

    Add CSS to components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Todos = ({ todos }) => {
     return (
-        <div>
+        <div style={styles.container}>
             {todos.map((todo) => {
                 return <p key={todo.id}>{todo.title}</p>
             })}
@@ -10,4 +10,10 @@ const Todos = ({ todos }) => {
     )
 }
 
+const styles = {
+    container: {
+        width: '40%',
+        margin: '0 auto',
+    },
+}
 export default Todos
